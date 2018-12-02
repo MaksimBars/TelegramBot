1e5b77f030f45311bee04256b800cf3c62864326
MaksimBars <4403604@gmail.com>
2018-12-02 09:12:12 +0300

    Added test data sampling.

--- script_db.py
+++ script_db.py
@@ -3,6 +3,7 @@ import parser
 
 
 def add_base(word):
+    """Функция добавления в базу данных транскрипцию и перевод полученую после парсинга сайта."""
     translate = parser.get_url(word)
     conn = sqlite3.connect('words.db')
     cursor = conn.cursor()
@@ -15,9 +16,28 @@ def add_base(word):
     # conn.close()
 
 
-def search_in_db(word):
-    pass
+def search_eng_in_db(word):
+    """Функция поиска дубликатов в базе перед добавлением в неё новых слов, возращает True если слово есть в базе, и
+    False если в ней нет такого слова."""
+    word = word.lower()
+    conn = sqlite3.connect('words.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT COUNT() FROM words WHERE eng = (?)", [word])
+    count = cursor.fetchone()[0]
+    if not count:
+        conn.commit()
+        conn.close()
+        return False
+    else:
+        conn.commit()
+        conn.close()
+        return True
 
 
-def data_sampling(word):
-    pass
+def data_sampling():
+    """Функция выборки рандомной строки из базы данных.(Позже передалется под алгоритм проверки и выборки)"""
+    conn = sqlite3.connect('words.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM words ORDER BY RANDOM() LIMIT 1")
+    cut = cursor.fetchall()[0]
+    return cut
