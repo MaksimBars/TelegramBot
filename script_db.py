3c05bce091a0e567fd433ebf533f9f75713cb632
MaksimBars <4403604@gmail.com>
2018-12-07 09:08:09 +0300

    Added search in db.

--- script_db.py
+++ script_db.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import sqlite3
 import parser
 
@@ -7,8 +8,8 @@ def add_base(word):
     translate = parser.get_url(word)
     conn = sqlite3.connect('words.db')
     cursor = conn.cursor()
-    cursor.execute('INSERT INTO words (eng, transcription, rus, right, wrong) VALUES (?, ?, ?, ?, ?)',
-                   (translate[0], translate[1], translate[2], 0, 0))
+    cursor.execute('INSERT INTO words (eng, transcription, rus, many_rus, properly, wrong) VALUES (?, ?, ?, ?, ?, ?)',
+                   (translate[0], translate[1], translate[2], translate[3], 0, 0))
     conn.commit()
     conn.close()
     # cursor.execute("""CREATE TABLE words ('eng','transcription','rus','right','wrong')""")
@@ -25,11 +26,9 @@ def search_eng_in_db(word):
     cursor.execute("SELECT COUNT() FROM words WHERE eng = (?)", [word])
     count = cursor.fetchone()[0]
     if not count:
-        conn.commit()
         conn.close()
         return False
     else:
-        conn.commit()
         conn.close()
         return True
 
@@ -39,5 +38,26 @@ def data_sampling():
     conn = sqlite3.connect('words.db')
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM words ORDER BY RANDOM() LIMIT 1")
-    cut = cursor.fetchall()[0]
+    cut = cursor.fetchone()
     return cut
+
+
+def search_rus_in_db(word, text):
+    """Поиск по русскому слову"""
+    conn = sqlite3.connect('words.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM words WHERE eng=? AND rus=?", [word, text])
+    count = cursor.fetchone()
+    if count:
+        return "Верно"
+    else:
+        text = "Неверно:\n" + search_in_db(word)
+        return text
+
+
+def search_in_db(word):
+    conn = sqlite3.connect('words.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM words WHERE eng=?", [word])
+    some_rus = cursor.fetchone()[3]
+    return some_rus
